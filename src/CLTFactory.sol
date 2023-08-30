f67a49a55a5d0b183c3eddb7958bb1116bd1e5d7
mudassir45 <mudassirali36@gmail.com>
2023-08-30 14:31:45 +0500

    add basic methods

--- src/CLTFactory.sol
+++ src/CLTFactory.sol
@@ -4,4 +4,6 @@ pragma solidity >=0.8.19;
 import "./CLTVault.sol";
 import "./interfaces/ICLTFactory.sol";
 
-contract CLTFactory is ICLTFactory { }
+contract CLTFactory is ICLTFactory {
+    function createVault() external { }
+}
--- src/CLTVault.sol
+++ src/CLTVault.sol
@@ -3,4 +3,11 @@ pragma solidity >=0.8.19;
 
 import "./interfaces/ICLTVault.sol";
 
-contract CLTVault is ICLTVault { }
+import "openzeppelin-contracts/token/ERC721/ERC721.sol";
+
+contract CLTVault is ICLTVault {
+    function deposit() external { }
+    function withdraw() external { }
+    function claim() external { }
+    function shiftLiquidity() external { }
+}
