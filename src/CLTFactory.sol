b40d8957089321fe8a7b184daa04f3891da5f3d5
Mudassir45 <Mudassir45@users.noreply.github.com>
2023-08-30 14:31:45 +0500

    feat: initial commit

---
+++ src/CLTFactory.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.19;
+
+import "./CLTVault.sol";
+import "./interfaces/ICLTFactory.sol";
+
+contract CLTFactory is ICLTFactory {
+    function createVault() external { }
+}
---
+++ src/CLTVault.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.19;
+
+import "./interfaces/ICLTVault.sol";
+
+import "openzeppelin-contracts/token/ERC721/ERC721.sol";
+
+contract CLTVault is ICLTVault {
+    function deposit() external { }
+    function withdraw() external { }
+    function claim() external { }
+    function shiftLiquidity() external { }
+}
