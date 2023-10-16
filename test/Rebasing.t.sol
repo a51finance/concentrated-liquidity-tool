36fddad2ed29b5ffadfa8762c8ae94ba2de73057
anassohail99 <anas.sohailabs1@gmail.com>
2023-10-16 17:35:32 +0500

    feat: add test cases

--- test/Rebasing.t.sol
+++ test/Rebasing.t.sol
@@ -6,13 +6,12 @@ import "forge-std/console.sol";
 
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
-
 import "../src/CLTBase.sol";
 import "../src/base/Structs.sol";
 import "../src/modules/rebasing/Preference.sol";
 
 contract RebasingModulesTest is Test {
-    Vm hevm = Vm(HEVM_ADDRESS);
+    Vm _hevm = Vm(HEVM_ADDRESS);
 
     address public WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public uniswapV3Factory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
@@ -21,20 +20,22 @@ contract RebasingModulesTest is Test {
     IUniswapV3Factory uniswapV3FactoryContract = IUniswapV3Factory(uniswapV3Factory);
     IUniswapV3Pool poolContract = IUniswapV3Pool(poolAddress);
 
-    bytes[] public exitStrategyData;
-    bytes[] public rebasePreferenceData = [abi.encode(1, 2, 4, 5, 67, 8)];
-    bytes[] public liquidityDistributionData;
+    StrategyKey public strategyKey;
 
-    uint64[] public exitStrategy;
-    uint64[] public rebasePreference = [1];
-    uint64[] public liquidityDistribution;
+    // bytes[] public exitStrategyData;
+    // bytes[] public rebasePreferenceData;
+    // bytes[] public liquidityDistributionData;
+
+    // uint64[] public exitStrategy;
+    // uint64[] public rebasePreference = [1];
+    // uint64[] public liquidityDistribution;
 
     CLTBase public baseContract;
     RebasePreference public rebasePreferenceContrat;
     PositionActions public positionActionsData;
     ActionsData actionsData;
 
-    function floor(int24 tick, int24 tickSpacing) internal pure returns (int24) {
+    function floor(int24 tick, int24 tickSpacing) public pure returns (int24) {
         int24 compressed = tick / tickSpacing;
         if (tick < 0 && tick % tickSpacing != 0) compressed--;
         return compressed * tickSpacing;
@@ -50,67 +51,75 @@ contract RebasingModulesTest is Test {
         );
 
         rebasePreferenceContrat = new RebasePreference(address(baseContract), address(this));
+    }
 
+    function createBaseContractStrategy(
+        bytes[] memory _exitStrategyData,
+        bytes[] memory _rebasePreferenceData,
+        bytes[] memory _liquidityDistributionData,
+        uint8 _mode,
+        uint64[] memory _exitStrategy,
+        uint64[] memory _rebasePreference,
+        uint64[] memory _liquidityDistribution
+    )
+        public
+    {
         (, int24 tick,,,,,) = poolContract.slot0();
 
         int24 tickLower = floor(tick - 2000, poolContract.tickSpacing());
         int24 tickUpper = floor(tick + 2000, poolContract.tickSpacing());
 
-        StrategyKey memory strategyKey;
         strategyKey.pool = poolContract;
         strategyKey.tickLower = tickLower;
         strategyKey.tickUpper = tickUpper;
 
-        actionsData.exitStrategyData = exitStrategyData;
-        actionsData.rebasePreferenceData = rebasePreferenceData;
-        actionsData.liquidityDistributionData = liquidityDistributionData;
+        actionsData.exitStrategyData = _exitStrategyData;
+        actionsData.rebasePreferenceData = _rebasePreferenceData;
+        actionsData.liquidityDistributionData = _liquidityDistributionData;
 
-        positionActionsData.mode = 2;
-        positionActionsData.exitStrategy = exitStrategy;
-        positionActionsData.rebasePreference = rebasePreference;
-        positionActionsData.liquidityDistribution = liquidityDistribution;
+        positionActionsData.mode = _mode;
+        positionActionsData.exitStrategy = _exitStrategy;
+        positionActionsData.rebasePreference = _rebasePreference;
+        positionActionsData.liquidityDistribution = _liquidityDistribution;
 
         baseContract.createStrategy(strategyKey, actionsData, positionActionsData, false);
-    }
-
-    function testCLTContractDeployment() public {
-        bytes32 strategyID = baseContract.getStrategyId(address(this), 1);
-        (StrategyKey memory key,,,,,,,) = baseContract.strategies(strategyID);
-
-        (, int24 tick,,,,,) = poolContract.slot0();
-
-        int24 tickLower = floor(tick - 2000, poolContract.tickSpacing());
-        int24 tickUpper = floor(tick + 2000, poolContract.tickSpacing());
-
-        assertEq(key.tickLower, tickLower);
-        assertEq(key.tickUpper, tickUpper);
-        assertEq(address(key.pool), address(poolContract));
     }
 
     function testCheckStrategies() public {
-        bytes32 strategyID = baseContract.getStrategyId(address(this), 1);
+        bytes32 strategyId = keccak256(abi.encode(address(this), 1));
         bytes32[] memory strategyIds = new bytes32[](1);
-        strategyIds[0] = strategyID;
+        strategyIds[0] = strategyId;
 
-        (, int24 tick,,,,,) = poolContract.slot0();
+        bytes[] memory rebasePreferenceData = new bytes[](1);
+        rebasePreferenceData[0] = abi.encode(1000, 1000, 2000, 2000);
 
-        int24 tickLower = floor(tick - 2000, poolContract.tickSpacing());
-        int24 tickUpper = floor(tick + 2000, poolContract.tickSpacing());
+        uint64[] memory rebasePreference = new uint64[](1);
+        rebasePreference[0] = 1;
 
-        StrategyKey memory strategyKey;
-        strategyKey.pool = poolContract;
-        strategyKey.tickLower = tickLower;
-        strategyKey.tickUpper = tickUpper;
-
-        rebasePreferenceData = [abi.encode(tickUpper + 4000, tickLower - 4000, 20, 30, 50, 50)];
-        actionsData.rebasePreferenceData = rebasePreferenceData;
-        baseContract.createStrategy(strategyKey, actionsData, positionActionsData, false);
-
-        bytes32 strategyID2 = baseContract.getStrategyId(address(this), 2);
-        console.logBytes32(strategyID);
-        console.logBytes32(strategyID2);
-
+        createBaseContractStrategy(
+            new bytes[](0), rebasePreferenceData, new bytes[](0), 2, new uint64[](0), rebasePreference, new uint64[](0)
+        );
         bytes32[] memory queue = rebasePreferenceContrat.checkStrategies(strategyIds);
-        console.log(queue.length);
+        assertEq(queue[0], strategyId);
+    }
+
+    function testExecuteStrategies() public {
+        bytes[] memory rebasePreferenceData = new bytes[](1);
+        rebasePreferenceData[0] = abi.encode(1000, 1000, 2000, 2000);
+
+        uint64[] memory rebasePreference = new uint64[](1);
+        rebasePreference[0] = 1;
+
+        createBaseContractStrategy(
+            new bytes[](0), rebasePreferenceData, new bytes[](0), 2, new uint64[](0), rebasePreference, new uint64[](0)
+        );
+
+        bytes32 strategyId = keccak256(abi.encode(address(this), 1));
+        bytes32[] memory strategyIds = new bytes32[](1);
+        strategyIds[0] = strategyId;
+        bytes32[] memory queue = rebasePreferenceContrat.checkStrategies(strategyIds);
+
+        rebasePreferenceContrat.toggleOperator(address(this));
+        rebasePreferenceContrat.executeStrategies(queue[0]);
     }
 }
