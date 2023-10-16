e854687bf0736554ecadfe382622312d346f2bfe
anassohail99 <anas.sohailabs1@gmail.com>
2023-10-16 17:34:52 +0500

    fix: remove struct

--- src/interfaces/modules/IPreference.sol
+++ src/interfaces/modules/IPreference.sol
@@ -1,15 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
-struct RebasePereferenceParams {
-    int24 upperPreference;
-    int24 lowerPreference;
-    int24 lowerBaseThreshold; // need to figure this out
-    int24 upperBaseThreshold; // need to figure this out
-    int8 upperPercentage;
-    int8 lowerPercentage;
-}
-
 interface IPreference {
     error InvalidCaller();
 }
