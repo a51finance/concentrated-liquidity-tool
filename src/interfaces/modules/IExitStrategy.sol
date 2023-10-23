d307f66ca44a86a0628630adeb8eae9bf69e7c12
mudassir45 <mudassirali36@gmail.com>
2023-10-23 18:49:36 +0500

    add checkInputData method

--- src/interfaces/modules/IExitStrategy.sol
+++ src/interfaces/modules/IExitStrategy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
-interface IPreference {
+interface IExitStrategy {
     error InvalidCaller();
 
     function checkInputData(bytes[] memory data) external;
--- src/interfaces/modules/ILiquidityDistribution.sol
+++ src/interfaces/modules/ILiquidityDistribution.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
-interface IPreference {
+interface ILiquidityDistribution {
     error InvalidCaller();
 
     function checkInputData(bytes[] memory data) external;
