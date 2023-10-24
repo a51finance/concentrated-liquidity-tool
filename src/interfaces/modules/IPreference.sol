5b37e35b42dff960f792dd8964c05d63d5868704
anassohail99 <anas.sohailabs1@gmail.com>
2023-10-24 13:29:47 +0500

    feat: add error logs

--- src/interfaces/modules/IPreference.sol
+++ src/interfaces/modules/IPreference.sol
@@ -5,10 +5,15 @@ interface IPreference {
     error InvalidCaller();
     error InvalidThreshold();
     error InvalidModesLength();
+    error InvalidMode();
+    error StrategyIdsCannotBeEmpty();
+    error StrategyIdCannotBeZero();
+    error DuplicateStrategyId(bytes32 strategyId);
 
     struct StrategyData {
         bytes32 strategyID;
         uint64[3] modes; // Array to hold multiple valid modes
     }
+
     function checkInputData(bytes[] memory data) external;
 }
