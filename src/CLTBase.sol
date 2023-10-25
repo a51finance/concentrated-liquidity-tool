c7bcd8c5332e4f8ecad350f007e431a1a2dab44d
anassohail99 <anas.sohailabs1@gmail.com>
2023-10-25 19:40:28 +0500

    feat: support for rebase inactivity

--- src/CLTBase.sol
+++ src/CLTBase.sol
@@ -122,6 +122,8 @@ contract CLTBase is ICLTBase, CLTPayments, Owned, ERC721 {
             actions: positionActionsHash,
             actionsData: actionsDataHash,
             isCompound: isCompound,
+            isRebaseActive: false,
+            inActivityThreshold: 0,
             balance0: 0,
             balance1: 0,
             totalShares: 0,
