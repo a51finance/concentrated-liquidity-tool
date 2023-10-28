d9a00a467341103dd24846da79f918594c03e0d0
anassohail99 <anas.sohailabs1@gmail.com>
2023-10-28 16:05:39 +0500

    refactor and optimize code

--- src/interfaces/modules/IPreference.sol
+++ src/interfaces/modules/IPreference.sol
@@ -7,16 +7,26 @@ interface IPreference {
     error InvalidModesLength();
     error InvalidMode();
     error InvalidPreferenceDifference();
+    error InvalidTimePreference();
     error StrategyIdsCannotBeEmpty();
     error StrategyIdCannotBeZero();
     error DuplicateStrategyId(bytes32 strategyId);
     error TimePreferenceConstraint();
     error BothTicksCannotBeZero();
     error RebaseStrategyDataCannotBeZero();
+    error OnlyRebaseInactivityCannotBeSelected();
+    error RebaseInactivityCannotBeZero();
 
     struct StrategyData {
         bytes32 strategyID;
         uint256[3] modes; // Array to hold multiple valid modes
+    }
+
+    enum Mode {
+        DUMMY,
+        REBASE_PREFERENCE,
+        REBASE_TIME_PREFERENCE,
+        REBASE_INACTIVITY
     }
 
     function checkInputData(bytes[] memory data) external returns (bool);
--- src/modules/rebasing/Preference.sol
+++ src/modules/rebasing/Preference.sol
@@ -8,7 +8,7 @@ import "../../interfaces/modules/IPreference.sol";
 import "../../interfaces/ICLTBase.sol";
 
 /// @title Rebase Module Contract
-contract RebaseModule is ModeTicksCalculation, AccessControl, ICLTBase, IPreference {
+contract RebaseModule is ModeTicksCalculation, AccessControl, IPreference {
     /// @notice Threshold for liquidity consideration
     uint256 public liquidityThreshold = 1e3;
     /// @notice Maximum allowable time period
@@ -17,11 +17,10 @@ contract RebaseModule is ModeTicksCalculation, AccessControl, ICLTBase, IPrefere
     ICLTBase _cltBase; // Instance of the ICLTBase interface
 
     /// @notice Constructs the RebaseModule with the provided parameters.
-    /// @param __cltBase Address of the CLTBase.
     /// @param _owner Address of the owner.
-    /// @param _baseContract Address of the base contract.
-    constructor(address __cltBase, address _owner, address _baseContract) AccessControl(_owner) {
-        _cltBase = ICLTBase(payable(_baseContract));
+    /// @param _baseContractAddress Address of the base contract.
+    constructor(address _owner, address _baseContractAddress) AccessControl(_owner) {
+        _cltBase = ICLTBase(payable(_baseContractAddress));
         maxTimePeriod = 31_536_000; // Represents seconds in a year.
     }
 
@@ -37,13 +36,13 @@ contract RebaseModule is ModeTicksCalculation, AccessControl, ICLTBase, IPrefere
             ShiftLiquidityParams memory params;
             (StrategyKey memory key,,, bytes memory actionStatus,,,,,,,) = _cltBase.strategies(_queue[i].strategyID);
             uint256 rebaseCount = abi.decode(actionStatus, (uint256));
+
             params.strategyId = _queue[i].strategyID;
             params.shouldMint = false;
             params.swapAmount = 0;
 
             for (uint256 j = 0; j < _queue[i].modes.length; j++) {
                 (int24 tickLower, int24 tickUpper) = getTicksForMode(key, _queue[i].modes[j]);
-                if (tickLower == 0 && tickUpper == 0) revert BothTicksCannotBeZero();
                 key.tickLower = tickLower;
                 key.tickUpper = tickUpper;
                 params.key = key;
@@ -60,6 +59,7 @@ contract RebaseModule is ModeTicksCalculation, AccessControl, ICLTBase, IPrefere
     /// @param key Strategy key.
     /// @param mode Mode to calculate ticks.
     /// @return tickLower and tickUpper values.
+
     function getTicksForMode(
         StrategyKey memory key,
         uint256 mode
@@ -68,21 +68,21 @@ contract RebaseModule is ModeTicksCalculation, AccessControl, ICLTBase, IPrefere
         view
         returns (int24 tickLower, int24 tickUpper)
     {
-        if (mode == 1) {
+        if (mode == uint256(Mode.REBASE_PREFERENCE)) {
             (tickLower, tickUpper) = shiftLeft(key);
-        } else if (mode == 2) {
+        } else if (mode == uint256(Mode.REBASE_TIME_PREFERENCE)) {
             (tickLower, tickUpper) = shiftRight(key);
-        } else if (mode == 3) {
+        } else if (mode == uint256(Mode.REBASE_INACTIVITY)) {
             (tickLower, tickUpper) = shiftBothSide(key);
         }
-        return (tickLower = 0, tickUpper = 0);
+        return (tickLower, tickUpper);
     }
 
     /// @notice Checks and processes strategies based on their validity.
     /// @dev Returns an array of valid strategies.
     /// @param strategyIDs Array of strategy IDs to check and process.
     /// @return StrategyData[] array containing valid strategies.
-    function checkAndProcessStrategies(bytes32[] memory strategyIDs) internal view returns (StrategyData[] memory) {
+    function checkAndProcessStrategies(bytes32[] memory strategyIDs) internal returns (StrategyData[] memory) {
         StrategyData[] memory _queue = new StrategyData[](strategyIDs.length);
         uint256 validEntries = 0;
 
@@ -98,16 +98,26 @@ contract RebaseModule is ModeTicksCalculation, AccessControl, ICLTBase, IPrefere
     /// @notice Retrieves strategy data based on strategy ID.
     /// @param strategyID The ID of the strategy to retrieve.
     /// @return StrategyData representing the retrieved strategy.
-    function getStrategyData(bytes32 strategyID) internal view returns (StrategyData memory) {
-        (StrategyKey memory key, bytes memory actions,, bytes memory actionStatus,,,, uint256 totalShares,,,) =
-            _cltBase.strategies(strategyID);
+    function getStrategyData(bytes32 strategyID) internal returns (StrategyData memory) {
+        (
+            StrategyKey memory key,
+            bytes memory actions,
+            bytes memory actionsData,
+            bytes memory actionStatus,
+            ,
+            ,
+            ,
+            uint256 totalShares,
+            ,
+            ,
+        ) = _cltBase.strategies(strategyID);
 
         if (totalShares <= liquidityThreshold) {
             return StrategyData(bytes32(0), [uint256(0), uint256(0), uint256(0)]);
         }
 
         PositionActions memory positionActionData = abi.decode(actions, (PositionActions));
-        ActionsData memory actionsData = abi.decode(actions, (ActionsData));
+        ActionsData memory _actionsData = abi.decode(actionsData, (ActionsData));
 
         if (positionActionData.rebaseStrategy.length > 2) {
             revert InvalidModesLength();
@@ -118,8 +128,8 @@ contract RebaseModule is ModeTicksCalculation, AccessControl, ICLTBase, IPrefere
         for (uint256 i = 0; i < positionActionData.rebaseStrategy.length; i++) {
             uint256 preference = positionActionData.rebaseStrategy[i];
 
-            if (_checkRebaseInactivityStrategies(actionsData, actionStatus)) {
-                if (shouldAddToQueue(preference, key, actionsData)) {
+            if (_checkRebaseInactivityStrategies(_actionsData, actionStatus)) {
+                if (shouldAddToQueue(preference, key, _actionsData)) {
                     data.modes[count++] = preference;
                 }
             }
@@ -168,8 +178,6 @@ contract RebaseModule is ModeTicksCalculation, AccessControl, ICLTBase, IPrefere
         view
         returns (bool)
     {
-        // rebase preference data will be encode with int24 and int24 types
-
         (int24 lowerPreferenceDiff, int24 upperPreferenceDiff) =
             abi.decode(actionsData.rebaseStrategyData[0], (int24, int24));
 
@@ -216,33 +224,50 @@ contract RebaseModule is ModeTicksCalculation, AccessControl, ICLTBase, IPrefere
         return true;
     }
 
-    /// @notice Checks the input data for validity.
-    /// @param data An array of encoded actions data.
-    /// @return true if the input data is valid.
-    function checkInputData(bytes[] memory data) external returns (bool) {
+    function checkInputData(bytes[] memory data) external view returns (bool) {
         for (uint256 i = 0; i < data.length; i++) {
             ActionsData memory actionsData = abi.decode(data[i], (ActionsData));
 
             if (actionsData.rebaseStrategyData.length == 0) {
                 revert RebaseStrategyDataCannotBeZero();
             }
-            if (isNonZero(actionsData.rebaseStrategyData[0])) {
+
+            bool hasDiffPreference = isNonZero(actionsData.rebaseStrategyData[0]);
+            bool hasTimePreference = isNonZero(actionsData.rebaseStrategyData[1]);
+            bool hasInActivity = isNonZero(actionsData.rebaseStrategyData[2]);
+
+            if (hasDiffPreference) {
                 (int24 lowerPreferenceDiff, int24 upperPreferenceDiff) =
                     abi.decode(actionsData.rebaseStrategyData[0], (int24, int24));
-
                 if (lowerPreferenceDiff <= 0 || upperPreferenceDiff <= 0) {
                     revert InvalidPreferenceDifference();
                 }
             }
-            if (isNonZero(actionsData.rebaseStrategyData[1])) { }
-            if (isNonZero(actionsData.rebaseStrategyData[2])) { }
+
+            if (hasTimePreference) {
+                uint256 timePreference = abi.decode(actionsData.rebaseStrategyData[1], (uint256));
+                if (timePreference <= block.timestamp || timePreference >= maxTimePeriod || timePreference == 0) {
+                    revert InvalidTimePreference();
+                }
+            }
+
+            if (hasInActivity) {
+                if (hasDiffPreference && hasTimePreference) {
+                    revert OnlyRebaseInactivityCannotBeSelected();
+                }
+                uint256 preferredInActivity = abi.decode(actionsData.rebaseStrategyData[2], (uint256));
+                if (preferredInActivity == 0) {
+                    revert RebaseInactivityCannotBeZero();
+                }
+            }
         }
+        return true;
     }
 
     /// @notice Checks the bytes value is non zero or not.
     /// @param data bytes value to be checked.
     /// @return true if the value is nonzero.
-    function isNonZero(bytes memory data) public pure returns (bool) {
+    function isNonZero(bytes memory data) internal pure returns (bool) {
         for (uint256 i = 0; i < data.length; i++) {
             if (data[i] != bytes1(0)) {
                 return true;
@@ -254,8 +279,8 @@ contract RebaseModule is ModeTicksCalculation, AccessControl, ICLTBase, IPrefere
     /// @notice Checks the strategies array for validity.
     /// @param data An array of strategy IDs.
     /// @return true if the strategies array is valid.
-    function checkStrategiesArray(bytes32[] memory data) public returns (bool) {
-        // check array length
+    function checkStrategiesArray(bytes32[] memory data) public pure returns (bool) {
+        // this function has a comlexity of O(n^2).
         if (data.length == 0) {
             revert StrategyIdsCannotBeEmpty();
         }
@@ -275,7 +300,9 @@ contract RebaseModule is ModeTicksCalculation, AccessControl, ICLTBase, IPrefere
         return true;
     }
 
-    /// @notice Calculates the preference ticks based on the strategy key and the given preference differences.
+    ///@notice Calculates the preference ticks based on the strategy key and the given preference differences.
+    /// @dev  This function adjusts the given tick bounds (both lower and upper) based on a preference difference. The
+    /// preference differences indicate by how much the ticks should be moved.
     /// @param _key The strategy key.
     /// @param lowerPreferenceDiff The lower preference difference.
     /// @param upperPreferenceDiff The upper preference difference.
@@ -296,6 +323,10 @@ contract RebaseModule is ModeTicksCalculation, AccessControl, ICLTBase, IPrefere
     }
 
     /// @notice Floors the given tick value based on the specified tick spacing.
+    /// @dev The flooring logic ensures that tick values are compliant with the tick spacing set by the pool.
+    /// This is especially necessary for protocols like Uniswap V3 where positions are defined by ticks and the ticks
+    /// have a specific spacing. If a tick is not an exact multiple of the spacing, this function helps to floor it
+    /// to the nearest lower multiple.
     /// @param tick The tick value to be floored.
     /// @param tickSpacing The spacing value for flooring.
     /// @return The floored tick value.
@@ -314,83 +345,4 @@ contract RebaseModule is ModeTicksCalculation, AccessControl, ICLTBase, IPrefere
         }
         liquidityThreshold = _newThreshold;
     }
-
-    /// @notice Fetches details of a particular strategy
-    /// @param strategyId The unique identifier for the strategy
-    /// @return key The key data of the strategy
-    /// @return actions The actions associated with the strategy
-    /// @return actionsData Additional data for the actions
-    /// @return actionStatus The status of each action
-    /// @return isCompound Boolean indicating if the strategy uses compounding
-    /// @return balance0 The balance of token 0
-    /// @return balance1 The balance of token 1
-    /// @return totalShares The total shares associated with this strategy
-    /// @return uniswapLiquidity The liquidity provided in Uniswap
-    /// @return feeGrowthInside0LastX128 The fee growth for token 0
-    /// @return feeGrowthInside1LastX128 The fee growth for token 1
-    function strategies(bytes32 strategyId)
-        external
-        override
-        returns (
-            StrategyKey memory key,
-            bytes memory actions,
-            bytes memory actionsData,
-            bytes memory actionStatus,
-            bool isCompound,
-            uint256 balance0,
-            uint256 balance1,
-            uint256 totalShares,
-            uint128 uniswapLiquidity,
-            uint256 feeGrowthInside0LastX128,
-            uint256 feeGrowthInside1LastX128
-        )
-    { }
-
-    /// @notice Fetches position details using the position identifier
-    /// @param positionId The unique identifier for the position
-    /// @return strategyId The strategy ID associated with this position
-    /// @return liquidityShare The liquidity share for this position
-    /// @return feeGrowthInside0LastX128 The fee growth for token 0
-    /// @return feeGrowthInside1LastX128 The fee growth for token 1
-    /// @return tokensOwed0 The tokens owed for token 0
-    /// @return tokensOwed1 The tokens owed for token 1
-    function positions(uint256 positionId)
-        external
-        override
-        returns (
-            bytes32 strategyId,
-            uint256 liquidityShare,
-            uint256 feeGrowthInside0LastX128,
-            uint256 feeGrowthInside1LastX128,
-            uint128 tokensOwed0,
-            uint128 tokensOwed1
-        )
-    { }
-
-    /// @notice Allows a user to deposit and create a new position
-    /// @param params The deposit parameters
-    /// @return tokenId The unique identifier for the position created
-    /// @return liquidity The liquidity amount
-    /// @return amount0 The amount of token 0 deposited
-    /// @return amount1 The amount of token 1 deposited
-    function deposit(DepositParams calldata params)
-        external
-        payable
-        override
-        returns (uint256 tokenId, uint256 liquidity, uint256 amount0, uint256 amount1)
-    { }
-
-    /// @notice Allows a user to withdraw from their position
-    /// @param params The withdrawal parameters
-    /// @return amount0 The amount of token 0 withdrawn
-    /// @return amount1 The amount of token 1 withdrawn
-    function withdraw(WithdrawParams calldata params) external override returns (uint256 amount0, uint256 amount1) { }
-
-    /// @notice Allows a user to claim fees from their position
-    /// @param params The parameters required to claim fees
-    function claimPositionFee(ClaimFeesParams calldata params) external override { }
-
-    /// @notice Allows a user to shift their liquidity
-    /// @param params The parameters required to shift liquidity
-    function shiftLiquidity(ShiftLiquidityParams calldata params) external override { }
 }
