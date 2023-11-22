43ed3a3a852629b22fb3b9eb3c400a91900dce34
mudassir45 <mudassirali36@gmail.com>
2023-11-22 16:09:57 +0500

    test: update fixtures

---
+++ test/StrategyCreation.t.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.15;
+
+import { Vm } from "forge-std/Vm.sol";
+import { Test } from "forge-std/Test.sol";
+import { CLTBase } from "../src/CLTBase.sol";
+import { Fixtures } from "./utils/Fixtures.sol";
+import { ICLTBase } from "../src/interfaces/ICLTBase.sol";
+
+import { IUniswapV3Pool } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+
+contract StrategyCreationTest is Test, Fixtures {
+    CLTBase base;
+    IUniswapV3Pool pool;
+    ICLTBase.StrategyKey key;
+
+    event StrategyCreated(
+        bytes32 indexed strategyId, ICLTBase.StrategyKey indexed key, bytes positionActions, bool isCompound
+    );
+
+    function setUp() public {
+        (base, pool) = initBase();
+
+        key = ICLTBase.StrategyKey({ pool: pool, tickLower: -100, tickUpper: 100 });
+    }
+
+    /// forge test -vv --match-test test_strategy_succeedsWithValidInputsForInactivity
+    function test_strategy_succeedsWithValidInputsForInactivity(uint256 mode, uint256 inactivityCounts) public {
+        vm.assume(mode > 0 && mode < 4);
+        vm.assume(inactivityCounts > 0);
+
+        ICLTBase.PositionActions memory actions = createStrategyActions(mode, 3, 0, inactivityCounts, 0, 0);
+
+        bytes32 strategyId = getStrategyID(0xDB8cFf278adCCF9E9b5da745B44E754fC4EE3C76, 1);
+
+        vm.expectEmit(true, true, false, true);
+        emit StrategyCreated(strategyId, key, abi.encode(actions), true);
+        base.createStrategy(key, actions, 10e15, true);
+
+        (ICLTBase.StrategyKey memory keyAdded, address owner, bytes memory actionsAdded,, bool isCompound,,,,,,) =
+            base.strategies(strategyId);
+
+        assertEq(isCompound, true);
+        assertEq(abi.encode(actions), actionsAdded);
+        assertEq(key.tickLower, keyAdded.tickLower);
+        assertEq(owner, 0xDB8cFf278adCCF9E9b5da745B44E754fC4EE3C76); // check msg.sender of foundry
+    }
+
+    function test_strategy_succeedsWithValidInputsForPreference() public { }
+
+    function test_strategy_succeedsWithValidInputsForTime() public { }
+
+    function test() public { }
+}
--- test/utils/Fixtures.sol
+++ test/utils/Fixtures.sol
@@ -1,14 +1,114 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity =0.8.15;
 
+import { WETH } from "@solmate/tokens/WETH.sol";
 import { ERC20Mock } from "../mocks/ERC20Mock.sol";
 
-contract Fixtures {
+import { CLTBase } from "../../src/CLTBase.sol";
+import { ICLTBase } from "../../src/interfaces/ICLTBase.sol";
+import { RebaseModule } from "../../src/modules/rebasing/RebaseModule.sol";
+
+import { UniswapDeployer } from "../lib/UniswapDeployer.sol";
+
+import { SwapRouter } from "@uniswap/v3-periphery/contracts/SwapRouter.sol";
+import { ISwapRouter } from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+
+import { TickMath } from "@uniswap/v3-core/contracts/libraries/TickMath.sol";
+import { IUniswapV3Pool } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import { IUniswapV3Factory } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+
+contract Fixtures is UniswapDeployer {
+    SwapRouter router;
+    WETH weth;
+
     function deployTokens(uint8 count, uint256 totalSupply) public returns (ERC20Mock[] memory tokens) {
         tokens = new ERC20Mock[](count);
         for (uint8 i = 0; i < count; i++) {
             tokens[i] = new ERC20Mock();
             tokens[i].mint(msg.sender, totalSupply);
         }
+    }
+
+    function initPool() internal returns (IUniswapV3Factory factory, IUniswapV3Pool pool) {
+        ERC20Mock[] memory tokens = deployTokens(2, 1e50);
+
+        // intialize uniswap contracts
+        factory = IUniswapV3Factory(deployUniswapV3Factory());
+        pool = IUniswapV3Pool(factory.createPool(address(tokens[0]), address(tokens[1]), 500));
+        pool.initialize(TickMath.getSqrtRatioAtTick(0));
+    }
+
+    function initBase() internal returns (CLTBase base, IUniswapV3Pool pool) {
+        IUniswapV3Factory factory;
+
+        (factory, pool) = initPool();
+
+        base = new CLTBase("ALP Base", "ALP", address(this), address(0), 10e14, factory);
+
+        RebaseModule rebaseModule = new RebaseModule(msg.sender, address(base));
+
+        base.addModule(keccak256("REBASE_STRATEGY"), keccak256("TIME_PREFERENCE"), address(rebaseModule), true);
+        base.addModule(keccak256("REBASE_STRATEGY"), keccak256("PRICE_PREFERENCE"), address(rebaseModule), true);
+        base.addModule(keccak256("REBASE_STRATEGY"), keccak256("REBASE_INACTIVITY"), address(rebaseModule), true);
+    }
+
+    function getStrategyID(address user, uint256 strategyCount) internal pure returns (bytes32 strategyID) {
+        strategyID = keccak256(abi.encode(user, strategyCount));
+    }
+
+    function createStrategyActions(
+        uint256 basicMode,
+        uint256 advanceMode,
+        uint256 timePreference,
+        uint256 inactivityCounts,
+        int24 lowerPreferenceDiff,
+        int24 upperPreferenceDiff
+    )
+        internal
+        pure
+        returns (ICLTBase.PositionActions memory actions)
+    {
+        ICLTBase.StrategyPayload[] memory rebaseStrategyActions;
+
+        if (advanceMode == 1) {
+            rebaseStrategyActions = new ICLTBase.StrategyPayload[](1);
+
+            rebaseStrategyActions[0].actionName = 0xca2ac00817703c8a34fa4f786a4f8f1f1eb57801f5369ebb12f510342c03f53b;
+            rebaseStrategyActions[0].data = abi.encode(lowerPreferenceDiff, upperPreferenceDiff);
+        }
+
+        if (advanceMode == 2) {
+            rebaseStrategyActions = new ICLTBase.StrategyPayload[](1);
+
+            rebaseStrategyActions[0].actionName = 0x4036d2cde3df45671689d4979c1a0416dd81c5761f9d35cce34ae9a59728ccb2;
+            rebaseStrategyActions[0].data = abi.encode(timePreference);
+        }
+
+        if (advanceMode == 3) {
+            rebaseStrategyActions = new ICLTBase.StrategyPayload[](1);
+
+            rebaseStrategyActions[0].actionName = 0x697d458f1054678eeb971e50a66090683c55cfb1cab904d3050bdfe6ab249893;
+            rebaseStrategyActions[0].data = abi.encode(inactivityCounts);
+        }
+
+        if (advanceMode == 4) {
+            rebaseStrategyActions = new ICLTBase.StrategyPayload[](3);
+
+            rebaseStrategyActions[0].actionName = 0xca2ac00817703c8a34fa4f786a4f8f1f1eb57801f5369ebb12f510342c03f53b;
+            rebaseStrategyActions[0].data = abi.encode(lowerPreferenceDiff, upperPreferenceDiff);
+
+            rebaseStrategyActions[1].actionName = 0x4036d2cde3df45671689d4979c1a0416dd81c5761f9d35cce34ae9a59728ccb2;
+            rebaseStrategyActions[1].data = abi.encode(timePreference);
+
+            rebaseStrategyActions[2].actionName = 0x697d458f1054678eeb971e50a66090683c55cfb1cab904d3050bdfe6ab249893;
+            rebaseStrategyActions[2].data = abi.encode(inactivityCounts);
+        }
+
+        actions = ICLTBase.PositionActions({
+            mode: basicMode,
+            exitStrategy: new ICLTBase.StrategyPayload[](0), // not available till now
+            rebaseStrategy: rebaseStrategyActions,
+            liquidityDistribution: new ICLTBase.StrategyPayload[](0) // not available till now
+         });
     }
 }
