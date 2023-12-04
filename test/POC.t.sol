f7511c27df230f848e36347f1630f45347a19de8
anassohail99 <anas.sohailabs1@gmail.com>
2023-12-04 13:19:37 +0500

    removed poc

--- test/POC.t.sol
+++ test/POC.t.sol
@@ -2,15 +2,11 @@
 pragma solidity =0.8.15;
 
 import { ICLTBase } from "../src/interfaces/ICLTBase.sol";
-import { IPreference } from "../src/interfaces/modules/IPreference.sol";
-
 import { RebaseModuleMock } from "./mocks/RebaseModule.mock.sol";
-import { ModeTicksCalculation } from "../src/base/ModeTicksCalculation.sol";
 import { Vm } from "forge-std/Vm.sol";
 import { Test } from "forge-std/Test.sol";
 import { CLTBase } from "../src/CLTBase.sol";
 import { console } from "forge-std/console.sol";
-
 import { WETH } from "@solmate/tokens/WETH.sol";
 import { ERC20Mock } from "./mocks/ERC20Mock.sol";
 import { UniswapDeployer } from "./lib/UniswapDeployer.sol";
@@ -20,8 +16,9 @@ import { SwapRouter } from "@uniswap/v3-periphery/contracts/SwapRouter.sol";
 import { ISwapRouter } from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import { TickMath } from "@uniswap/v3-core/contracts/libraries/TickMath.sol";
 import { IUniswapV3Pool } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
-import { RebaseFixtures } from "./utils/RebaseModuleFixtures.sol";
 import { IUniswapV3Factory } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import { IGovernanceFeeHandler } from "../src/interfaces/IGovernanceFeeHandler.sol";
+import { GovernanceFeeHandler } from "../src/GovernanceFeeHandler.sol";
 
 contract POC is Test, UniswapDeployer {
     Vm _hevm = Vm(HEVM_ADDRESS);
@@ -30,6 +27,7 @@ contract POC is Test, UniswapDeployer {
     IUniswapV3Factory uniswapV3FactoryContract;
     RebaseModuleMock rebaseModuleMockContract;
     IUniswapV3Pool poolContract;
+    GovernanceFeeHandler feeHandler;
     INonfungiblePositionManager.MintParams mintParams;
     CLTBase baseContract;
     SwapRouter router;
@@ -87,8 +85,18 @@ contract POC is Test, UniswapDeployer {
 
         positionManager.mint(mintParams);
 
+        IGovernanceFeeHandler.ProtocolFeeRegistry memory feeParams = IGovernanceFeeHandler.ProtocolFeeRegistry({
+            lpAutomationFee: 0,
+            strategyCreationFee: 0,
+            protcolFeeOnManagement: 0,
+            protcolFeeOnPerformance: 0
+        });
+
+        feeHandler = new GovernanceFeeHandler(address(this), feeParams, feeParams);
+
         // initialize base contract
-        baseContract = new CLTBase("ALP Base", "ALP", owner, address(0), 1000000000000000, uniswapV3FactoryContract);
+        baseContract =
+            new CLTBase("ALP Base", "ALP", owner, address(weth),  address(feeHandler),uniswapV3FactoryContract);
 
         // approve tokens
         token0.approve(address(baseContract), type(uint256).max);
@@ -178,106 +186,106 @@ contract POC is Test, UniswapDeployer {
         return compressed * tickSpacing;
     }
 
-    function testPOC() public {
-        ICLTBase.StrategyPayload[] memory rebaseActions = new ICLTBase.StrategyPayload[](1);
-        rebaseActions[0].actionName = rebaseModuleMockContract.REBASE_INACTIVITY();
-        rebaseActions[0].data = abi.encode(3);
+    // function testPOC() public {
+    //     ICLTBase.StrategyPayload[] memory rebaseActions = new ICLTBase.StrategyPayload[](1);
+    //     rebaseActions[0].actionName = rebaseModuleMockContract.REBASE_INACTIVITY();
+    //     rebaseActions[0].data = abi.encode(3);
 
-        ICLTBase.PositionActions memory positionActions;
+    //     ICLTBase.PositionActions memory positionActions;
 
-        getStrategyKey(1000);
+    //     getStrategyKey(1000);
 
-        positionActions.mode = 1;
-        positionActions.exitStrategy = new ICLTBase.StrategyPayload[](0);
-        positionActions.rebaseStrategy = rebaseActions;
-        positionActions.liquidityDistribution = new ICLTBase.StrategyPayload[](0);
+    //     positionActions.mode = 1;
+    //     positionActions.exitStrategy = new ICLTBase.StrategyPayload[](0);
+    //     positionActions.rebaseStrategy = rebaseActions;
+    //     positionActions.liquidityDistribution = new ICLTBase.StrategyPayload[](0);
 
-        baseContract.createStrategy(strategyKey, positionActions, 0, false, true);
+    //     baseContract.createStrategy(strategyKey, positionActions, 0, 0, false, true);
 
-        ICLTBase.DepositParams memory depositParams;
+    //     ICLTBase.DepositParams memory depositParams;
 
-        depositParams.strategyId = keccak256(abi.encode(address(this), 1));
-        depositParams.amount0Desired = 100e18;
-        depositParams.amount1Desired = 100e18;
-        depositParams.amount0Min = 0;
-        depositParams.amount1Min = 0;
-        depositParams.recipient = alice;
+    //     depositParams.strategyId = keccak256(abi.encode(address(this), 1));
+    //     depositParams.amount0Desired = 100e18;
+    //     depositParams.amount1Desired = 100e18;
+    //     depositParams.amount0Min = 0;
+    //     depositParams.amount1Min = 0;
+    //     depositParams.recipient = alice;
 
-        _hevm.prank(alice);
-        baseContract.deposit(depositParams);
+    //     _hevm.prank(alice);
+    //     baseContract.deposit(depositParams);
 
-        executeSwap(token0, token1, 500, address(this), 10e18, 0, 0);
-        executeSwap(token1, token0, 500, address(this), 10e18, 0, 0);
+    //     executeSwap(token0, token1, 500, address(this), 10e18, 0, 0);
+    //     executeSwap(token1, token0, 500, address(this), 10e18, 0, 0);
 
-        // user 2
+    //     // user 2
 
-        baseContract.createStrategy(strategyKey, positionActions, 0, true, true);
+    //     baseContract.createStrategy(strategyKey, positionActions, 0, 0, true, true);
 
-        depositParams.strategyId = keccak256(abi.encode(address(this), 2));
-        depositParams.amount0Desired = 10e18;
-        depositParams.amount1Desired = 10e18;
-        depositParams.amount0Min = 0;
-        depositParams.amount1Min = 0;
-        depositParams.recipient = bob;
+    //     depositParams.strategyId = keccak256(abi.encode(address(this), 2));
+    //     depositParams.amount0Desired = 10e18;
+    //     depositParams.amount1Desired = 10e18;
+    //     depositParams.amount0Min = 0;
+    //     depositParams.amount1Min = 0;
+    //     depositParams.recipient = bob;
 
-        _hevm.prank(bob);
-        baseContract.deposit(depositParams);
+    //     _hevm.prank(bob);
+    //     baseContract.deposit(depositParams);
 
-        // executeSwap(token0, token1, 500, address(this), 20e18, 0, 0);
-        // executeSwap(token1, token0, 500, address(this), 20e18, 0, 0);
+    //     // executeSwap(token0, token1, 500, address(this), 20e18, 0, 0);
+    //     // executeSwap(token1, token0, 500, address(this), 20e18, 0, 0);
 
-        _hevm.prank(address(baseContract));
-        poolContract.burn(strategyKey.tickLower, strategyKey.tickUpper, 0);
+    //     _hevm.prank(address(baseContract));
+    //     poolContract.burn(strategyKey.tickLower, strategyKey.tickUpper, 0);
 
-        // alice
+    //     // alice
 
-        (,,, uint128 fee0, uint128 fee1) = poolContract.positions(
-            keccak256(abi.encodePacked(address(baseContract), strategyKey.tickLower, strategyKey.tickUpper))
-        );
+    //     (,,, uint128 fee0, uint128 fee1) = poolContract.positions(
+    //         keccak256(abi.encodePacked(address(baseContract), strategyKey.tickLower, strategyKey.tickUpper))
+    //     );
 
-        (, uint256 liquidityShareAlice,,,,) = baseContract.positions(1);
-        (, uint256 liquidityShareBob,,,,) = baseContract.positions(2);
+    //     (, uint256 liquidityShareAlice,,,,) = baseContract.positions(1);
+    //     (, uint256 liquidityShareBob,,,,) = baseContract.positions(2);
 
-        console.log("===============Alice (no Compound)===============");
-        console.log("Strategy ID");
-        console.logBytes32(keccak256(abi.encode(address(this), 1)));
-        console.log("Upper Tick");
-        console.logInt(strategyKey.tickUpper);
-        console.log("Lower Tick");
-        console.logInt(strategyKey.tickLower);
-        console.log("Alice Deposit token0", 100e18);
-        console.log("Alice Deposit token1", 100e18);
-        console.log("Alice Share", liquidityShareAlice);
+    //     console.log("===============Alice (no Compound)===============");
+    //     console.log("Strategy ID");
+    //     console.logBytes32(keccak256(abi.encode(address(this), 1)));
+    //     console.log("Upper Tick");
+    //     console.logInt(strategyKey.tickUpper);
+    //     console.log("Lower Tick");
+    //     console.logInt(strategyKey.tickLower);
+    //     console.log("Alice Deposit token0", 100e18);
+    //     console.log("Alice Deposit token1", 100e18);
+    //     console.log("Alice Share", liquidityShareAlice);
 
-        console.log("===============Bob (Compound)===============");
-        console.log("Strategy ID");
-        console.logBytes32(keccak256(abi.encode(address(this), 2)));
-        console.log("Upper Tick");
-        console.logInt(strategyKey.tickUpper);
-        console.log("Lower Tick");
-        console.logInt(strategyKey.tickLower);
-        console.log("Bob Deposit token0", 10e18);
-        console.log("Bob Deposit token1", 10e18);
-        console.log("Bob Share", liquidityShareBob);
+    //     console.log("===============Bob (Compound)===============");
+    //     console.log("Strategy ID");
+    //     console.logBytes32(keccak256(abi.encode(address(this), 2)));
+    //     console.log("Upper Tick");
+    //     console.logInt(strategyKey.tickUpper);
+    //     console.log("Lower Tick");
+    //     console.logInt(strategyKey.tickLower);
+    //     console.log("Bob Deposit token0", 10e18);
+    //     console.log("Bob Deposit token1", 10e18);
+    //     console.log("Bob Share", liquidityShareBob);
 
-        console.log("=============================================");
+    //     console.log("=============================================");
 
-        console.log("Total Fees 0", fee0);
-        console.log("Total Fees 1", fee1);
+    //     console.log("Total Fees 0", fee0);
+    //     console.log("Total Fees 1", fee1);
 
-        console.log("===============Bob Withdraws===============");
+    //     console.log("===============Bob Withdraws===============");
 
-        _hevm.prank(bob);
-        baseContract.withdraw(
-            ICLTBase.WithdrawParams({ tokenId: 2, liquidity: liquidityShareBob, recipient: bob, refundAsETH: true })
-        );
+    //     _hevm.prank(bob);
+    //     baseContract.withdraw(
+    //         ICLTBase.WithdrawParams({ tokenId: 2, liquidity: liquidityShareBob, recipient: bob, refundAsETH: true })
+    //     );
 
-        _hevm.prank(address(baseContract));
-        (,,, fee0, fee1) = poolContract.positions(
-            keccak256(abi.encodePacked(address(baseContract), strategyKey.tickLower, strategyKey.tickUpper))
-        );
+    //     _hevm.prank(address(baseContract));
+    //     (,,, fee0, fee1) = poolContract.positions(
+    //         keccak256(abi.encodePacked(address(baseContract), strategyKey.tickLower, strategyKey.tickUpper))
+    //     );
 
-        console.log("Total Fees 0 after bob withdraws:", fee0);
-        console.log("Total Fees 1 after bob withdraws:", fee1);
-    }
+    //     console.log("Total Fees 0 after bob withdraws:", fee0);
+    //     console.log("Total Fees 1 after bob withdraws:", fee1);
+    // }
 }
