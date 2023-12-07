5df5857146c9ea7e29ffe45baa441bf0ef44e578
mudassir45 <mudassirali36@gmail.com>
2023-12-07 08:35:02 +0500

    test: add increaseLiquidity setup

---
+++ test/CLTUpdatePositionLiquidity.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.15;
+
+import { Vm } from "forge-std/Vm.sol";
+import { Test } from "forge-std/Test.sol";
+import { CLTBase } from "../src/CLTBase.sol";
+import { Fixtures } from "./utils/Fixtures.sol";
+import { ICLTBase } from "../src/interfaces/ICLTBase.sol";
+
+import { IUniswapV3Pool } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+
+contract CLTUpdatePositionLiquidityTest is Test, Fixtures { }
