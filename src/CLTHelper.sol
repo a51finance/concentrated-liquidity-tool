454dc2372ab194a892caa46fe7ac1941391536dd
anassohail99 <anas.sohailabs1@gmail.com>
2023-12-07 17:38:11 +0500

    helper contract

---
+++ src/CLTHelper.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity =0.8.15;
+
+import { ICLTBase } from "./interfaces/ICLTBase.sol";
+
+contract CLTHelper {
+    function decodePositionActions(bytes memory actions) external pure returns (ICLTBase.PositionActions memory) {
+        ICLTBase.PositionActions memory modules = abi.decode(actions, (ICLTBase.PositionActions));
+        return modules;
+    }
+}
