73177c34ab5924f67efbf25e85c302a0f39973b2
anassohail99 <anas.sohailabs1@gmail.com>
2023-12-08 11:47:30 +0500

    removed interface directory

--- src/CLTHelper.sol
+++ src/CLTHelper.sol
@@ -1,11 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity =0.8.15;
 
-import { ICLTBase } from "./interfaces/ICLTBase.sol";
+struct StrategyPayload {
+    bytes32 actionName;
+    bytes data;
+}
+
+struct PositionActions {
+    uint256 mode;
+    StrategyPayload[] exitStrategy;
+    StrategyPayload[] rebaseStrategy;
+    StrategyPayload[] liquidityDistribution;
+}
 
 contract CLTHelper {
-    function decodePositionActions(bytes memory actions) external pure returns (ICLTBase.PositionActions memory) {
-        ICLTBase.PositionActions memory modules = abi.decode(actions, (ICLTBase.PositionActions));
+    function decodePositionActions(bytes memory actions) external pure returns (PositionActions memory) {
+        PositionActions memory modules = abi.decode(actions, (PositionActions));
         return modules;
     }
 }
