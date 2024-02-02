50b3b085d5cb83b8711b3fe92d9d512a74382849
anassohail99 <anas.sohailabs1@gmail.com>
2024-02-02 14:47:03 +0500

    add simulation updates

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -12,32 +12,32 @@ import "../src/modules/rebasing/RebaseModule.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 
 contract DeployALP is Script {
-    address _owner = 0x97fF40b5678D2234B1E5C894b5F39b8BA8535431;
-    address _weth9 = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
+    // address _owner = 0x97fF40b5678D2234B1E5C894b5F39b8BA8535431;
+    // address _weth9 = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
 
     // mainnet
-    // address _owner = 0x9De199457b5F6e4690eac92c399A0Cd31B901Dc3;
-    // address _weth9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address _owner = 0x9De199457b5F6e4690eac92c399A0Cd31B901Dc3;
+    address _weth9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     IUniswapV3Factory _factoryAddress = IUniswapV3Factory(0x1F98431c8aD98523631AE4a59f267346ea31F984);
 
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_2");
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        CLTModules cltModules = new CLTModules(_owner);
+        // CLTModules cltModules = new CLTModules(_owner);
 
-        IGovernanceFeeHandler.ProtocolFeeRegistry memory feeParams = IGovernanceFeeHandler.ProtocolFeeRegistry({
-            lpAutomationFee: 0,
-            strategyCreationFee: 0,
-            protcolFeeOnManagement: 0,
-            protcolFeeOnPerformance: 0
-        });
+        // IGovernanceFeeHandler.ProtocolFeeRegistry memory feeParams = IGovernanceFeeHandler.ProtocolFeeRegistry({
+        //     lpAutomationFee: 0,
+        //     strategyCreationFee: 0,
+        //     protcolFeeOnManagement: 0,
+        //     protcolFeeOnPerformance: 0
+        // });
 
-        GovernanceFeeHandler feeHandler = new GovernanceFeeHandler(_owner, feeParams, feeParams);
+        // GovernanceFeeHandler feeHandler = new GovernanceFeeHandler(_owner, feeParams, feeParams);
 
-        CLTBase baseContract = new CLTBase("ALP_TOKEN", "ALPT", _owner,_weth9, address(feeHandler),
-        address(cltModules),
+        CLTBase baseContract = new CLTBase("ALP_TOKEN", "ALPT", _owner,_weth9, address(0xf2bBec7C8A7eb3D703f38618e16c6E0369010b97),
+        address(0xC88476C909EFa1853a44Ca12f0370929c7812dd8),
         _factoryAddress);
 
         new CLTHelper();
--- script/contract.js
+++ script/contract.js
@@ -6,13 +6,14 @@ const RebaseModuleABI = require("../out/RebaseModule.sol/RebaseModule.json");
 
 require("dotenv").config();
 
-const web3 = new Web3("https://eth-goerli.g.alchemy.com/v2/oD-Ft2zD3u6MzlCs5b7x-LzTK0OAL5--");
-const contractAddressBase = "0x8b23A5008303D31f709009FF99794389ed04A8b9";
-const contractAddressCLTModules = "0x8402Aebbc0b1b62c8c9F7AFafE95467394414711";
-const contractAddressRebaseModule = "0x4c8873A3ccC683f4893ac23a9F15f3D462A8D420";
+const web3 = new Web3("https://virtual.mainnet.rpc.tenderly.co/d1adf8c2-1e1c-4193-9a78-dc9df644db3e");
+const contractAddressBase = "0x23733faF3aed2c71F2430D638069BD17381294dF";
+const contractAddressCLTModules = "0xf506458ebdeb67d946cb8817005457db0d993212";
+const contractAddressRebaseModule = "0xe5b89c31a367afcda71e233b1371e54d5b78d24e";
 const MAX_UINT256 = "115792089237316195423570985008687907853269984665640564039457584007913129639935";
-const token0 = "0x3c5d67de8e81d65caa5552f5260d6986d4ea9f88";
-const token1 = "0xc71d5bc18438903dc9fff9b88088ac2d2bbea2e1";
+
+const token0 = "0x6b175474e89094c44da98b954eedeac495271d0f";
+const token1 = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
 
 const contractABIBase = CLTABI.abi;
 const baseContract = new web3.eth.Contract(contractABIBase, contractAddressBase);
@@ -37,11 +38,20 @@ const rebasePricePrefernece = "0xca2ac00817703c8a34fa4f786a4f8f1f1eb57801f5369eb
 const rebaseInactivity = "0x697d458f1054678eeb971e50a66090683c55cfb1cab904d3050bdfe6ab249893";
 
 // Define the parameters for createStrategy
+
+
+// const strategyKey = {
+//   pool: "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640",
+//   tickLower: "197050",
+//   tickUpper: "201050",
+// };
+
 const strategyKey = {
-  pool: "0xb810a57202388b59102feBf04dEfd0051Fe0344A",
-  tickLower: "140",
-  tickUpper: "340",
+  pool: "0x5777d92f208679db4b9778590fa3cab3ac9e2168",
+  tickLower: "-278954",
+  tickUpper: "-272802",
 };
+
 const positionActions = {
   exitStrategy: [],
   liquidityDistribution: [],
@@ -49,12 +59,12 @@ const positionActions = {
   rebaseStrategy: [
     {
       actionName: "0xca2ac00817703c8a34fa4f786a4f8f1f1eb57801f5369ebb12f510342c03f53b",
-      data: "0x00000000000000000000000000000000000000000000000000000000000000c80000000000000000000000000000000000000000000000000000000000000086",
+      data: "0x00000000000000000000000000000000000000000000000000000000000003040000000000000000000000000000000000000000000000000000000000000d44",
     },
   ],
 };
-const managementFees = 0;
-const performanceFees = 0;
+const managementFees = "100000000000000000";
+const performanceFees = "50000000000000000";
 const isCompound = true;
 const isPrivate = false;
 
@@ -83,6 +93,7 @@ async function executeCreateStrategy() {
 
     console.log("Transaction successful:", receipt);
     console.log("Strategy ID:", receipt.logs[0].topics[1]);
+    return receipt.logs[0].topics[1].toString();
   } catch (error) {
     console.error("Error executing createStrategy:", error);
   }
@@ -92,8 +103,8 @@ async function approveTokens() {
   const txn0 = ercContractToken0.methods.approve(contractAddressBase, MAX_UINT256);
   const txn1 = ercContractToken1.methods.approve(contractAddressBase, MAX_UINT256);
 
-  const gas0 = await txn0.estimateGas({ from: fromAddressA89 });
-  const gas1 = await txn1.estimateGas({ from: fromAddressA89 });
+  const gas0 = await txn0.estimateGas({ from: fromAddress });
+  const gas1 = await txn1.estimateGas({ from: fromAddress });
   const gasPrice = await web3.eth.getGasPrice();
 
   const txData0 = {
@@ -110,31 +121,38 @@ async function approveTokens() {
     gasPrice,
   };
 
-  const signedTx0 = await web3.eth.accounts.signTransaction(txData0, privateKeyA89);
+  const signedTx0 = await web3.eth.accounts.signTransaction(txData0, privateKey);
   const receipt0 = await web3.eth.sendSignedTransaction(signedTx0.rawTransaction);
   console.log("Transaction successful:", receipt0);
 
-  const signedTx1 = await web3.eth.accounts.signTransaction(txData1, privateKeyA89);
+  const signedTx1 = await web3.eth.accounts.signTransaction(txData1, privateKey);
   const receipt1 = await web3.eth.sendSignedTransaction(signedTx1.rawTransaction);
   console.log("Transaction successful receipt0:", receipt0);
   console.log("Transaction successful receipt1:", receipt1);
 }
 
-async function deposit() {
+async function deposit(strategyId) {
   try {
-    const depoitAmount0 = "1000000000";
-    const depoitAmount1 = "1000000000";
+    const balance0 = await ercContractToken0.methods.balanceOf(fromAddress).call();
+    const balance1 = await ercContractToken1.methods.balanceOf(fromAddress).call();
 
-    const depositTx = baseContract.methods.deposit(
-      "0x353fd513ce55139191f81b229e521f66addb59b7f3501b73a107801c611309e1",
-      depoitAmount0,
-      depoitAmount1,
-      depoitAmount0,
-      depoitAmount1,
-      fromAddressA89,
-    );
+    if (balance0 == 0 || balance1 == 0) {
+      throw "Insufficient funds";
+    }
 
-    const gas = await depositTx.estimateGas({ from: fromAddressA89 });
+    const depoitAmount0 = "5000";
+    const depoitAmount1 = "500";
+
+    const depositTx = baseContract.methods.deposit({
+      strategyId: strategyId,
+      amount0Desired: depoitAmount0,
+      amount1Desired: depoitAmount1,
+      amount0Min: depoitAmount0,
+      amount1Min: depoitAmount1,
+      recipient: fromAddress,
+    });
+
+    const gas = await depositTx.estimateGas({ from: fromAddress });
     const gasPrice = await web3.eth.getGasPrice();
 
     const txData = {
@@ -144,7 +162,74 @@ async function deposit() {
       gasPrice,
     };
 
-    const signedTx = await web3.eth.accounts.signTransaction(txData, privateKeyA89);
+    const signedTx = await web3.eth.accounts.signTransaction(txData, privateKey);
+    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+    console.log("Transaction successful:", receipt);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updatePositon() {
+  try {
+    const balance0 = await ercContractToken0.methods.balanceOf(fromAddress).call();
+    const balance1 = await ercContractToken1.methods.balanceOf(fromAddress).call();
+
+    if (balance0 == 0 || balance1 == 0) {
+      throw "Insufficient funds";
+    }
+
+    const depoitAmount0 = "500000000";
+    const depoitAmount1 = "500000000000000000000";
+    const tokenId = 1;
+
+    const depositTx = baseContract.methods.updatePositionLiquidity({
+      tokenId,
+      amount0Desired: depoitAmount0,
+      amount1Desired: depoitAmount1,
+    });
+
+    const gas = await depositTx.estimateGas({ from: fromAddress });
+    const gasPrice = await web3.eth.getGasPrice();
+
+    const txData = {
+      to: contractAddressBase,
+      data: depositTx.encodeABI(),
+      gas,
+      gasPrice,
+    };
+
+    const signedTx = await web3.eth.accounts.signTransaction(txData, privateKey);
+    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+    console.log("Transaction successful:", receipt);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function withdrawPosition() {
+  try {
+    const positionDetails = await baseContract.methods.positions(1).call();
+    const withdrawParams = {
+      tokenId: 1,
+      liquidity: "100",
+      recipient: fromAddress,
+      refundAsETH: false,
+    };
+
+    const depositTx = baseContract.methods.withdraw(withdrawParams);
+
+    const gas = await depositTx.estimateGas({ from: fromAddress });
+    const gasPrice = await web3.eth.getGasPrice();
+
+    const txData = {
+      to: contractAddressBase,
+      data: depositTx.encodeABI(),
+      gas,
+      gasPrice,
+    };
+
+    const signedTx = await web3.eth.accounts.signTransaction(txData, privateKey);
     const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
     console.log("Transaction successful:", receipt);
   } catch (error) {
@@ -213,11 +298,41 @@ async function txnData() {
   console.log(block.logs[0].topics[1]);
 }
 
+async function getStrategyDetails() {
+  const strategyId = "0x353fd513ce55139191f81b229e521f66addb59b7f3501b73a107801c611309e1";
+  const details = await baseContract.methods.strategies(strategyId).call();
+  console.log(details);
+}
+
+async function getPositionData() {
+  const position = 1;
+  const positionDetails = await baseContract.methods.positions(1).call();
+  console.log(positionDetails);
+}
+
+async function getBlockDetails() {
+  console.log(await web3.eth.getBlock(19110518));
+  console.log(await web3.eth.getBlock(19110525))
+}
+
+// async function init(){
+//   const strategyId = executeCreateStrategy();
+//   approveTokens();
+//   deposit(strategyId);
+// }
+
+
+// init();
+// getBlockDetails();
 // txnData();
-executeCreateStrategy();
+// executeCreateStrategy();
 // addModulesTxn();
 // addModulesVaultTxn();
 // checkModule();
 // checkOwner();
-// deposit();
+// deposit("0x353fd513ce55139191f81b229e521f66addb59b7f3501b73a107801c611309e1");
+// updatePositon();
 // approveTokens();
+// getStrategyDetails();
+withdrawPosition();
+// getPositionData();
