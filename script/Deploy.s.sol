f6c1e32962460cb148d4d36366216605c45205f3
mudassir45 <mudassirali36@gmail.com>
2024-03-16 16:06:45 +0500

    update script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -13,19 +13,20 @@ import "../src/modules/rebasing/RebaseModule.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 
 contract DeployALP is Script {
-    address _owner = 0x97fF40b5678D2234B1E5C894b5F39b8BA8535431;
-    address _weth9 = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
+    // address _owner = 0x97fF40b5678D2234B1E5C894b5F39b8BA8535431;
+    // address _weth9 = 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6;
 
     // mainnet
-    // address _owner = 0x9De199457b5F6e4690eac92c399A0Cd31B901Dc3;
-    // address _weth9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address _owner = 0x4eF03f0eA9e744F22B768E17628cE39a2f48AbE5;
+    address _weth9 = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
 
     IUniswapV3Factory _factoryAddress = IUniswapV3Factory(0x1F98431c8aD98523631AE4a59f267346ea31F984);
 
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_MAIN");
         vm.startBroadcast(deployerPrivateKey);
 
+        new CLTHelper();
         CLTModules cltModules = new CLTModules(_owner);
         CLTTwapQuoter twapQuoter = new CLTTwapQuoter(_owner);
 
@@ -38,10 +39,16 @@ contract DeployALP is Script {
 
         GovernanceFeeHandler feeHandler = new GovernanceFeeHandler(_owner, feeParams, feeParams);
 
-        CLTBase baseContract =
-            new CLTBase("ALP_TOKEN", "ALPT", _owner, _weth9, address(feeHandler), address(cltModules), _factoryAddress);
+        CLTBase baseContract = new CLTBase(
+            "A51 Liquidity Positions NFT",
+            "ALPhy",
+            _owner,
+            _weth9,
+            address(feeHandler),
+            address(cltModules),
+            _factoryAddress
+        );
 
-        new CLTHelper();
         new Modes(address(baseContract), address(twapQuoter), _owner);
         new RebaseModule(_owner, address(baseContract), address(twapQuoter));
 
