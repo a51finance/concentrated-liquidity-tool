82d521d1c661e1ba273d66cc02fdd9e882710322
mudassir45 <mudassirali36@gmail.com>
2024-10-29 19:24:39 +0500

    add ZapIn

---
+++ src/CLTZapIn.sol
@@ -0,0 +1,278 @@
+//SPDX-License-Identifier: MIT
+pragma solidity =0.8.20;
+
+import { Multicall } from "./libraries/Multicall.sol";
+import { AccessControl } from "./base/AccessControl.sol";
+import { ICLTBase } from "./interfaces/ICLTBase.sol";
+import { IWETH9 } from "./interfaces/external/IWETH9.sol";
+
+import { ERC20 } from "solmate/tokens/ERC20.sol";
+import { SafeTransferLib } from "solmate/utils/SafeTransferLib.sol";
+
+/// @title A51 Finance Autonomous Liquidity Provision ZapIn Contract
+/// @author undefined_0x
+/// @notice This contract is part of the A51 Finance platform, focusing on providing liquidity provision using single
+/// and whitelisted assets
+contract CLTZapIn is Multicall, AccessControl {
+    using SafeTransferLib for ERC20;
+
+    /// @notice Error thrown when the same token is used for both input and output
+    error SameToken();
+    /// @notice Error thrown when the transaction deadline has passed
+    error PastDeadline();
+    /// @notice Error thrown when the OKX swap fails
+    error OKXSwapFailed();
+    /// @notice Error thrown when the output amount is insufficient
+    error InsufficientOutput();
+    /// @notice Error thrown when the input amount is invalid
+    error InvalidAmountInput();
+    /// @notice Error thrown when invalid tokenId is passed
+    error InvalidTokenId();
+
+    /// @notice Event emitted when a ZapIn operation is completed
+    /// @param zapper The address of the user who initiated the ZapIn
+    /// @param tokenId The token ID of the liquidity position
+    /// @param share The share of the liquidity position
+    /// @param amount0 The amount of token0 deposited
+    /// @param amount1 The amount of token1 deposited
+    event ZapInCompleted(address zapper, uint256 tokenId, uint256 share, uint256 amount0, uint256 amount1);
+
+    /// @notice The Odos proxy contract used for swaps
+    address public immutable ODOS_ROUTER_V2;
+
+    /// @notice CLTBase contract for managing Uniswap v3 liquidity
+    ICLTBase private immutable CLT_BASE;
+
+    /// @notice The Wrapped Ethereum contract
+    IWETH9 public immutable WETH;
+
+    /// @notice Constructor to initialize the CLTZappIn contract
+    /// @param odosRouterV2 The address of the OKX proxy contract
+    /// @param cltBase The address of the CLTBase contract
+    constructor(address odosRouterV2, ICLTBase cltBase, IWETH9 weth, address owner) AccessControl(owner) {
+        ODOS_ROUTER_V2 = odosRouterV2;
+        CLT_BASE = cltBase;
+        WETH = weth;
+    }
+
+    /// @notice Performs a ZapIn operation by depositing tokens into the CLTBase contract
+    /// @param depositParams The deposit parameters for the CLTBase contract
+    /// @param token0 The first token to deposit
+    /// @param token1 The second token to deposit
+    /// @param useContractBalance0 Whether to use the contract's balance of token0
+    /// @param useContractBalance1 Whether to use the contract's balance of token1
+    /// @return tokenId The token ID of the liquidity position
+    /// @return share The share of the liquidity position
+    /// @return amount0 The amount of token0 deposited
+    /// @return amount1 The amount of token1 deposited
+    function zapIn(
+        ICLTBase.DepositParams memory depositParams,
+        ERC20 token0,
+        ERC20 token1,
+        bool useContractBalance0,
+        bool useContractBalance1
+    )
+        external
+        payable
+        virtual
+        nonReentrancy
+        whenNotPaused
+        returns (uint256 tokenId, uint256 share, uint256 amount0, uint256 amount1)
+    {
+        depositParams.amount0Desired = _getDesiredAmount(token0, depositParams.amount0Desired, useContractBalance0);
+        depositParams.amount1Desired = _getDesiredAmount(token1, depositParams.amount1Desired, useContractBalance1);
+
+        if (depositParams.amount0Desired != 0) {
+            token0.safeApprove(address(CLT_BASE), depositParams.amount0Desired);
+        }
+        if (depositParams.amount1Desired != 0) {
+            token1.safeApprove(address(CLT_BASE), depositParams.amount1Desired);
+        }
+
+        // Call deposit on cltBase
+        (tokenId, share, amount0, amount1) = CLT_BASE.deposit(depositParams);
+
+        // reset approvals
+        if (depositParams.amount0Desired != 0 && token0.allowance(address(this), address(CLT_BASE)) != 0) {
+            token0.safeApprove(address(CLT_BASE), 0);
+        }
+        if (depositParams.amount1Desired != 0 && token1.allowance(address(this), address(CLT_BASE)) != 0) {
+            token1.safeApprove(address(CLT_BASE), 0);
+        }
+
+        // Refund any excess tokens back to msg.sender
+        _refundTokens(token0, msg.sender);
+        _refundTokens(token1, msg.sender);
+
+        emit ZapInCompleted(msg.sender, tokenId, share, amount0, amount1);
+    }
+
+    /// @notice Performs a ZapIncrease operation by increasing position liquidity into the CLTBase contract
+    /// @param updateParams The update liquidity parameters for the CLTBase contract
+    /// @param token0 The first token to deposit
+    /// @param token1 The second token to deposit
+    /// @param useContractBalance0 Whether to use the contract's balance of token0
+    /// @param useContractBalance1 Whether to use the contract's balance of token1
+    /// @return share The share of the liquidity position
+    /// @return amount0 The amount of token0 deposited
+    /// @return amount1 The amount of token1 deposited
+    function zapIncrease(
+        ICLTBase.UpdatePositionParams memory updateParams,
+        ERC20 token0,
+        ERC20 token1,
+        bool useContractBalance0,
+        bool useContractBalance1
+    )
+        external
+        payable
+        virtual
+        nonReentrancy
+        whenNotPaused
+        returns (uint256 share, uint256 amount0, uint256 amount1)
+    {
+        updateParams.amount0Desired = _getDesiredAmount(token0, updateParams.amount0Desired, useContractBalance0);
+        updateParams.amount1Desired = _getDesiredAmount(token1, updateParams.amount1Desired, useContractBalance1);
+
+        if (updateParams.amount0Desired != 0) {
+            token0.safeApprove(address(CLT_BASE), updateParams.amount0Desired);
+        }
+        if (updateParams.amount1Desired != 0) {
+            token1.safeApprove(address(CLT_BASE), updateParams.amount1Desired);
+        }
+
+        updateParams.amount0Desired = updateParams.amount0Desired;
+        updateParams.amount1Desired = updateParams.amount1Desired;
+        updateParams.amount0Min = updateParams.amount0Min;
+        updateParams.amount1Min = updateParams.amount1Min;
+
+        (share, amount0, amount1) = CLT_BASE.updatePositionLiquidity(updateParams);
+
+        // reset approvals
+        if (updateParams.amount0Desired != 0 && token0.allowance(address(this), address(CLT_BASE)) != 0) {
+            token0.safeApprove(address(CLT_BASE), 0);
+        }
+        if (updateParams.amount1Desired != 0 && token1.allowance(address(this), address(CLT_BASE)) != 0) {
+            token1.safeApprove(address(CLT_BASE), 0);
+        }
+
+        // Refund any excess tokens back to msg.sender
+        _refundTokens(token0, msg.sender);
+        _refundTokens(token1, msg.sender);
+
+        emit ZapInCompleted(msg.sender, updateParams.tokenId, share, amount0, amount1);
+    }
+
+    /// @notice Determines the desired amount of tokens to be used in the deposit.
+    /// @param token The ERC20 token for which the desired amount is calculated.
+    /// @param desiredAmount The amount of tokens desired for the deposit.
+    /// @param useContractBalance A boolean indicating whether to use the contract's balance of the token.
+    /// @return The amount of tokens to be used for the deposit.
+    function _getDesiredAmount(
+        ERC20 token,
+        uint256 desiredAmount,
+        bool useContractBalance
+    )
+        internal
+        returns (uint256)
+    {
+        if (!useContractBalance) {
+            if (desiredAmount != 0) {
+                _handleTokenOperations(token, desiredAmount, address(this));
+            }
+        } else {
+            desiredAmount = token.balanceOf(address(this));
+        }
+        return desiredAmount;
+    }
+
+    /// @notice Performs a swap using the OKX proxy contract
+    /// @param tokenIn The input token for the swap
+    /// @param tokenAmountIn The amount of the input token
+    /// @param tokenOut The output token for the swap
+    /// @param minAmountOut The minimum amount of the output token expected
+    /// @param recipient The recipient of the output tokens
+    /// @param refundRecipient The recipient of any excess input tokens
+    /// @param deadline The deadline by which the swap must complete
+    /// @param swapData The swap data for the OKX proxy
+    /// @return tokenAmountOut The amount of the output token received
+    function executeSwap(
+        ERC20 tokenIn,
+        uint256 tokenAmountIn,
+        ERC20 tokenOut,
+        uint256 minAmountOut,
+        address recipient,
+        address refundRecipient,
+        bool useContractBalance,
+        uint256 deadline,
+        bytes calldata swapData
+    )
+        external
+        payable
+        virtual
+        nonReentrancy
+        whenNotPaused
+        returns (uint256 tokenAmountOut)
+    {
+        if (tokenIn == tokenOut) revert SameToken();
+        if (tokenAmountIn == 0) revert InvalidAmountInput();
+        if (block.timestamp > deadline) revert PastDeadline();
+
+        // Transfer tokenIn to the contract
+        if (!useContractBalance) {
+            _handleTokenOperations(tokenIn, tokenAmountIn, address(this));
+        }
+
+        // Approve the token amount for the OKX proxy
+        tokenIn.safeApprove(ODOS_ROUTER_V2, tokenAmountIn);
+
+        // Execute swap
+        (bool success,) = ODOS_ROUTER_V2.call(swapData);
+        if (!success) revert OKXSwapFailed();
+
+        // reset approvals
+        if (tokenIn.allowance(address(this), address(ODOS_ROUTER_V2)) != 0) {
+            tokenIn.safeApprove(address(ODOS_ROUTER_V2), 0);
+        }
+
+        // check slippage
+        tokenAmountOut = tokenOut.balanceOf(address(this));
+        if (tokenAmountOut < minAmountOut) revert InsufficientOutput();
+
+        // Transfer output tokens to recipient
+        if (recipient != address(this)) {
+            _handleTokenOperations(tokenOut, tokenAmountOut, recipient);
+        }
+
+        // Refund any excess input tokens
+        _refundTokens(tokenIn, refundRecipient);
+
+        return tokenAmountOut;
+    }
+
+    /// @notice Wraps the user's ETH input into WETH
+    /// @dev Should be used as part of a multicall to convert the user's ETH input into WETH
+    /// so that it can be swapped into other tokens.
+    function wrapEthInput() external payable nonReentrancy whenNotPaused {
+        WETH.deposit{ value: msg.value }();
+    }
+
+    /// @notice Handles token operations such as transfers and approvals
+    /// @param _token The token to operate on
+    /// @param _amountDesired The desired amount of the token
+    /// @param _to The recipient address
+    function _handleTokenOperations(ERC20 _token, uint256 _amountDesired, address _to) private {
+        if (_amountDesired > 0) {
+            _token.safeTransferFrom(msg.sender, _to, _amountDesired);
+        }
+    }
+
+    /// @notice Refunds any excess tokens back to the recipient
+    /// @param _token The token to refund
+    /// @param _recipient The recipient of the refund
+    function _refundTokens(ERC20 _token, address _recipient) private {
+        uint256 balance = _token.balanceOf(address(this));
+        if (balance > 0) {
+            _token.safeTransfer(_recipient, balance);
+        }
+    }
+}
---
+++ test/ZapIn.t.sol
@@ -0,0 +1,131 @@
+//SPDX-License-Identifier: MIT
+pragma solidity =0.8.20;
+
+import "forge-std/Test.sol";
+import "../src/CLTZapIn.sol";
+import "../src/interfaces/ICLTBase.sol";
+import { IAlgebraPool } from "@cryptoalgebra/integral-core/contracts/interfaces/IAlgebraPool.sol";
+
+import { ERC20 } from "solmate/tokens/ERC20.sol";
+import "forge-std/console.sol";
+import "../src/interfaces/external/IWETH9.sol";
+
+contract ZappInTest is Test {
+    // forge test --mt "test_basicAdd" -f "https://arbitrum.gateway.tenderly.co/8VBhOlPk9E6ONw0rbuYgi"
+    // f616d064-3cb8-4343-97e6-81e7772e04e4
+
+    CLTZapIn zapIn; // 0x74226579ED541adA94582DC4cD6DDd21f6526863;
+    ERC20 constant token0 = ERC20(0x4200000000000000000000000000000000000006); // USDC
+    ERC20 constant token1 = ERC20(0xd988097fb8612cc24eeC14542bC03424c656005f); // WETH
+
+    IWETH9 constant weth = IWETH9(0x4200000000000000000000000000000000000006);
+
+    address cltBase = 0x69317029384c3305fC04670c68a2b434e2D8C44C;
+
+    IAlgebraPool constant algebraPool = IAlgebraPool(0xbE3aD6a5669Dc0B8b12FeBC03608860C31E2eef6);
+    IAlgebraPool constant algebraPool2 = IAlgebraPool(0x25BA258E510FACA5Ab7Ff941A1584bDD2174c94D); //ETH-USDC
+
+    bytes32 strategyID = 0x4fa677272f9c7c30913c6c388ffc13912b19e21592867194c2f69e022d1760c3;
+    bytes32 strategyID2 = 0xd7521d176416b0a820d34e41c3a65845a3339c50093e8533b96a62b6a25ec867; //ETH-USDC
+
+    function setUp() public {
+        zapIn = new CLTZapIn({
+            odosRouterV2: 0x7E15EB462cdc67Cf92Af1f7102465a8F8c784874,
+            cltBase: ICLTBase(cltBase),
+            weth: weth,
+            owner: address(this)
+        });
+        console.log("ZapIn Address", address(zapIn));
+    }
+
+    function approveToken(uint256 amount, ERC20 token, address to) internal {
+        token.approve(to, amount);
+    }
+
+    function test_basicAdd() external {
+        uint256 amount0Desired = 1e18;
+        uint256 amount1Desired = 2622 * 1e6;
+
+        // mint tokens
+        deal(address(token0), address(this), amount0Desired);
+        deal(address(token1), address(this), amount1Desired);
+
+        approveToken(amount0Desired, token0, address(zapIn));
+        approveToken(amount1Desired, token1, address(zapIn));
+
+        console.log("Token0 Allowance Before", token0.allowance(address(zapIn), address(cltBase)));
+        console.log("Token1 Allowance Before", token1.allowance(address(zapIn), address(cltBase)));
+
+        (uint256 shares,,,) = zapIn.zapIn(
+            ICLTBase.DepositParams({
+                strategyId: strategyID2,
+                amount0Desired: amount0Desired,
+                amount1Desired: amount1Desired,
+                amount0Min: 0,
+                amount1Min: 0,
+                recipient: address(this)
+            }),
+            token0,
+            token1,
+            false,
+            false
+        );
+
+        console.log("Token0 Allowance After", token0.allowance(address(zapIn), address(cltBase)));
+        console.log("Token1 Allowance After", token1.allowance(address(zapIn), address(cltBase)));
+
+        assertGt(shares, 0, "shares are zero");
+        assertEq(token0.balanceOf(address(zapIn)), 0, "zap has token0 balance");
+        assertEq(token1.balanceOf(address(zapIn)), 0, "zap has token1 balance");
+    }
+
+    function test__multicallSwap__zappIn() public {
+        uint256 totalAmount1 = 3000 * 1e6;
+        uint256 amount1Desired = totalAmount1 / 2;
+
+        deal(address(token1), address(zapIn), totalAmount1);
+
+        approveToken(totalAmount1, token1, address(zapIn));
+
+        bytes[] memory calls = new bytes[](2);
+        calls[0] = abi.encodeWithSelector(
+            zapIn.executeSwap.selector,
+            address(token1),
+            amount1Desired,
+            address(token0),
+            0,
+            address(zapIn),
+            address(zapIn),
+            true,
+            block.timestamp + 1 days,
+            hex"84a7f3dd0101000156c85a254DD12eE8D9C04049a4ab62769Ce982100b7b45caa7149ab8000000000001d988097fb8612cc24eec14542bc03424c656005f0459682f0000000000040faddd9100015615dEB798BB3E4dFa0139dFa1b3D433Cc23b72f0000000004019b57dca972db5d8866c630554acdbdfe58b2659c0000000301020300060100010200020400000001ff000000000000000000000000000000468cc91df6f669cae6cdce766995bd7874052fbcd988097fb8612cc24eec14542bc03424c656005f00000000000000000000000000000000000000000000000000000000000000"
+        );
+
+        calls[1] = abi.encodeWithSelector(
+            zapIn.zapIn.selector,
+            ICLTBase.DepositParams({
+                strategyId: strategyID2,
+                amount0Desired: 5 ether,
+                amount1Desired: amount1Desired, // amount1 to be fetched by ratio
+                amount0Min: 0,
+                amount1Min: 0,
+                recipient: address(this)
+            }),
+            token0,
+            token1,
+            true,
+            true
+        );
+
+        console.logBytes(calls[0]);
+        console.logBytes(calls[1]);
+
+        bytes[] memory results = zapIn.multicall{ value: 5 ether }(calls);
+
+        (uint256 shares,,,) = abi.decode(results[1], (uint256, uint128, uint256, uint256));
+
+        console.log("share -> ", shares);
+        assertEq(token0.balanceOf(address(zapIn)), 0);
+        assertEq(token1.balanceOf(address(zapIn)), 0);
+    }
+}
